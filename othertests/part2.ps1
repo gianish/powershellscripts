54f04dfb25ca15e89e963d2b47e211872d7c2b7a
anish king <anish@govinvest.com>
2018-06-13 11:50:20 -0700

    download + run plans

--- othertests/part2.ps1
+++ othertests/part2.ps1
@@ -3,14 +3,16 @@
 # downloads demo plan pkg from promdev
 # runs for x amt of yrs
 
-
+$workDir = "C:\Users\anish\Documents\Prometheus Work June 2018\working week2 june\powershellscripts\othertests"
 $years = 5
 
 function download-plan($workDir) {
-    promctl downloadplan --
+    promctl downloadplan --env:promdev --output:"$workDir\results" --planId:Demo.City.General.v1.0
 }
 
 function run-plan($workDir) {
-    promctl calc --input:$workDir --output:"$workDir\results" --years:$years
+    promctl calc --input:"$workDir\results" --output:"$workDir\results" --years:$years
 }
 
+download-plan $workDir
+run-plan $workDir
