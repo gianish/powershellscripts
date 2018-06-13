bd619980b039504f24bf011144ff2ad38760b6de
anish king <anish@govinvest.com>
2018-06-13 14:45:48 -0700

    begin ex 3

---
+++ othertests/myscript.ps1
@@ -0,0 +1,21 @@
+#myscript.ps1 <filename> <text-to-search> <replacing-text>
+#anish king
+#june 2018
+
+
+
+function replace-text(){
+    $r = $r.Replace()
+}
+
+replace-text $FLAG
+
+
+
+
+
+
+
+
+
+
