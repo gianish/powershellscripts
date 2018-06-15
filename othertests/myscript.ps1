9bf2f5bce4736259c0556da524fe3b188487bc38
anish king <anish@govinvest.com>
2018-06-15 10:51:13 -0700

    test with file: file1.txt. original file contents for repeatable testing located at myfile.txt. the current method uses a combo of .NET classes, regex and system IO, which is similar to what is used in the prep.ps1 file located in the vacaville opeb folders. this problem is DONE. however, as a review, the characters look odd when viewed in the sublime environment, but appear to print the correct result to console / vim. go figure, but here's an improvement possibility.

--- othertests/myscript.ps1
+++ othertests/myscript.ps1
@@ -2,9 +2,19 @@
 #anish king
 #june 2018
 
-$var1 = $args[0] 
-$var2 = $args[1] 
+$var0 = $args[0] 
+$var1 = $args[1] 
+$var2 = $args[2]
 
-get-content myfile.txt
-#foreach-object {$_ -replace '$var1', '$var2'} |
-#out-file myfile.txt
+write-host $var0
+write-host $var1
+write-host $var2
+
+#(Get-Content $var0) |
+#Foreach-Object {$_ -replace '\[$FLAG\]','youyou'}  | 
+#Set-Content $var0
+
+#Get-Content $var0
+
+$content = [System.IO.File]::ReadAllText("C:\Users\anish\Documents\Prometheus Work June 2018\working week2 june\powershellscripts\othertests\$var0").Replace("$var1","$var2")
+[System.IO.File]::WriteAllText("C:\Users\anish\Documents\Prometheus Work June 2018\working week2 june\powershellscripts\othertests\$var0", $content)
